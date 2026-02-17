4023ec165da2bdfd24a76d60763a92a73e97f1e4
Alessandro <81766448+SkyAle-bit@users.noreply.github.com>
2026-02-17 12:51:05 +0100

    Add files via upload

---
+++ tesi/tesi/src/main/java/com/project/tesi/TesiApplication.java
@@ -0,0 +1,13 @@
+package com.project.tesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesiApplication.class, args);
+	}
+
+}
---
+++ tesi/tesi/src/test/java/com/project/tesi/TesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
